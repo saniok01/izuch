7f9568f21047c86da95c64cc6a5bc16fae98064e
Tilt <riova01@gmail.ru>
2023-05-21 21:41:29 +0200

    первый код

--- izuch.js
+++ izuch.js
@@ -1,1 +1,18 @@
 `use strict`;
+const number0Films = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: number0Films,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
